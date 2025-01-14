59c13de254b14b7ed05f237be20f529d3de6954b
silver-saint <naydenovneyko@gmail.com>
2025-01-14 17:11:23 +0200

    refactored a bit

--- src/main.c
+++ src/main.c
@@ -4,60 +4,62 @@
 #include "player.h"
 
 
-const uint32_t WINDOW_WIDTH = 800;
-const uint32_t WINDOW_HEIGHT = 600;
-const uint32_t PADDLE_WIDTH = 25;
-const uint32_t PADDLE_HEIGHT = 100;
-const uint32_t PADDING = 50;
-int32_t PLAYER_POSITION_Y = 0; 
-const uint32_t PADDLE_SPEED = 5;
-
-
-int main() {
-    
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define PADDLE_WIDTH = 25
+#define PADDLE_HEIGHT = 100
+#define PADDING = 50
+float PLAYER_POSITION_Y = 0; 
+#define PADDLE_SPEED = 50;
+int InitSDL(SDL_Window* win, SDL_Renderer* renderer, SDL_Surface* surface)
+{
+ if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("SDL_Init Error: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_Window *win = SDL_CreateWindow("Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-    if (!win) {
-        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-        SDL_Quit();
         return -1;
     }
-    SDL_Renderer* renderer = SDL_CreateRenderer(win, 0, SDL_RENDERER_ACCELERATED);
+
+    win = SDL_CreateWindow("Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!win) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return -1;
+    }
+    renderer = SDL_CreateRenderer(win, 0, SDL_RENDERER_ACCELERATED);
     if(!renderer)
     {
         printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-        SDL_Quit();
         return -1;
     }
 
-    SDL_Surface* surface = SDL_GetWindowSurface(win);
+    surface = SDL_GetWindowSurface(win);
     if(!surface)
     {
         printf("SDL_CreateSurface Error: %s\n", SDL_GetError());
-        SDL_Quit();
+        return -1;
     }
-    
+    return 1;
+}
+int main() 
+{
+    SDL_Window *win = NULL;
+    SDL_Renderer* renderer = NULL;
+    SDL_Surface* surface = NULL;
+
+   if(InitSDL(win, renderer, surface) != 1)
+   {
+        SDL_Quit();
+        return -1;
+   }
     
     SDL_Rect p1;
     SDL_Rect p2;
     SDL_Event ev;
     int quit = 0;
-    uint64_t now = SDL_GetPerformanceCounter();
-    uint64_t last = 0;
-    double_t dt = 0.0;
-
+    float dt = 1.0f / 60.0f;
+    float dir = 0;
     while(!quit)
     {
     while (SDL_PollEvent(&ev))
     {
-        last = now;
-        now = SDL_GetPerformanceCounter();
-        dt = (now - last)/ (double_t)SDL_GetPerformanceFrequency();
-
         switch(ev.type)
         {
             case SDL_KEYDOWN:
@@ -66,33 +68,35 @@ int main() {
                 {
                     case SDLK_w:
                     {
-                        PLAYER_POSITION_Y-= (PADDLE_SPEED * dt * 80.0);
+                       dir = -1;
                     }
                     break;
                     case SDLK_s:
                     {
-                        PLAYER_POSITION_Y+= (PADDLE_SPEED * dt * 80.0);
+                        dir = 1;
                     }
                     break;
                 }
                 break;
+                case SDL_QUIT:
+                {
+                    quit = 1;
+                    break;
+                }
             }
-            case SDL_QUIT:
-            {
-                quit = 1;
-                break;
-            }
-
         }
+        PLAYER_POSITION_Y +=  (dir * dt);
+
+        //render fun;
         SDL_SetRenderDrawColor(renderer, 0x00,0x00,0x00,0x00);
         SDL_RenderClear(renderer);
         SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
-        DrawPlayer(&p1, (WINDOW_HEIGHT / 2 - PADDING) + PLAYER_POSITION_Y, 0, PADDLE_WIDTH, PADDLE_HEIGHT);
-        DrawPlayer(&p2, (WINDOW_HEIGHT / 2 - PADDING), (WINDOW_WIDTH - PADDLE_WIDTH), PADDLE_WIDTH, PADDLE_HEIGHT);
+        DrawPlayer(&p1, 0, (WINDOW_HEIGHT / 2 - PADDING) + PLAYER_POSITION_Y, PADDLE_WIDTH, PADDLE_HEIGHT);
+        DrawPlayer(&p2, (WINDOW_WIDTH - PADDLE_WIDTH + 1), (WINDOW_HEIGHT / 2 - PADDING), PADDLE_WIDTH, PADDLE_HEIGHT);
         SDL_RenderFillRect( renderer, &p1);
         SDL_RenderFillRect( renderer, &p2);
         SDL_RenderPresent(renderer);
-    }
+        }
     }
     SDL_DestroyWindow(win);
     SDL_Quit();
--- src/player.c
+++ src/player.c
@@ -3,8 +3,8 @@
 
 void DrawPlayer(SDL_Rect* player, int startPosX, int startPosY, int Width, int Height)
 {
-    player->y = startPosX;
-    player->x = startPosY;
+    player->x = startPosX;
+    player->y = startPosY;
     player->w = Width;
     player->h = Height;
 }
