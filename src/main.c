f4a37e2ce9812d14bd13c6b066a6f72ed1e6121a
silver-saint <naydenovneyko@gmail.com>
2025-01-14 18:11:21 +0200

    refactored a bit

--- src/main.c
+++ src/main.c
@@ -3,95 +3,49 @@
 #include <stdint.h>
 #include "player.h"
 
-
+//window
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
-#define PADDLE_WIDTH = 25
-#define PADDLE_HEIGHT = 100
-#define PADDING = 50
+//paddle size
+#define PADDLE_WIDTH 25
+#define PADDLE_HEIGHT 100
+#define PADDING 50
+#define PADDLE_SPEED 50
+//movement
 float PLAYER_POSITION_Y = 0; 
-#define PADDLE_SPEED = 50;
-int InitSDL(SDL_Window* win, SDL_Renderer* renderer, SDL_Surface* surface)
-{
- if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL_Init Error: %s\n", SDL_GetError());
-        return -1;
-    }
+//has game ended
+int quit = 0;
 
-    win = SDL_CreateWindow("Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-    if (!win) {
-        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-        return -1;
-    }
-    renderer = SDL_CreateRenderer(win, 0, SDL_RENDERER_ACCELERATED);
-    if(!renderer)
-    {
-        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
-        return -1;
-    }
 
-    surface = SDL_GetWindowSurface(win);
-    if(!surface)
-    {
-        printf("SDL_CreateSurface Error: %s\n", SDL_GetError());
-        return -1;
-    }
-    return 1;
-}
+
+int InitSDL(SDL_Window** win, SDL_Renderer** renderer, SDL_Surface** surface);
+void HandleInput(SDL_Event* ev);
 int main() 
 {
-    SDL_Window *win = NULL;
+    SDL_Window* win = NULL;
     SDL_Renderer* renderer = NULL;
     SDL_Surface* surface = NULL;
-
-   if(InitSDL(win, renderer, surface) != 1)
+    int32_t init = InitSDL(&win, &renderer, &surface);
+   if(!init)
    {
         SDL_Quit();
         return -1;
    }
-    
     SDL_Rect p1;
     SDL_Rect p2;
     SDL_Event ev;
-    int quit = 0;
-    float dt = 1.0f / 60.0f;
-    float dir = 0;
     while(!quit)
     {
     while (SDL_PollEvent(&ev))
     {
-        switch(ev.type)
-        {
-            case SDL_KEYDOWN:
-            {
-                switch (ev.key.keysym.sym)
-                {
-                    case SDLK_w:
-                    {
-                       dir = -1;
-                    }
-                    break;
-                    case SDLK_s:
-                    {
-                        dir = 1;
-                    }
-                    break;
-                }
-                break;
-                case SDL_QUIT:
-                {
-                    quit = 1;
-                    break;
-                }
-            }
-        }
-        PLAYER_POSITION_Y +=  (dir * dt);
+        HandleInput(&ev);
+      
 
         //render fun;
-        SDL_SetRenderDrawColor(renderer, 0x00,0x00,0x00,0x00);
+        SDL_SetRenderDrawColor(renderer, 0x00000000,0x0000000000,0x0000000000,0x0000000000);
         SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF );
-        DrawPlayer(&p1, 0, (WINDOW_HEIGHT / 2 - PADDING) + PLAYER_POSITION_Y, PADDLE_WIDTH, PADDLE_HEIGHT);
+        SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        DrawPlayer(&p1, 0, (WINDOW_HEIGHT / 2 - PADDING), PADDLE_WIDTH, PADDLE_HEIGHT);
         DrawPlayer(&p2, (WINDOW_WIDTH - PADDLE_WIDTH + 1), (WINDOW_HEIGHT / 2 - PADDING), PADDLE_WIDTH, PADDLE_HEIGHT);
         SDL_RenderFillRect( renderer, &p1);
         SDL_RenderFillRect( renderer, &p2);
@@ -101,4 +55,59 @@ int main()
     SDL_DestroyWindow(win);
     SDL_Quit();
     return 0;
+}
+
+
+int InitSDL(SDL_Window** win, SDL_Renderer** renderer, SDL_Surface** surface)
+{
+ if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    *win = SDL_CreateWindow("Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!(*win)) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return -1;
+    }
+    *renderer = SDL_CreateRenderer(*win, 0, SDL_RENDERER_ACCELERATED);
+    if(!(*renderer))
+    {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    *surface = SDL_GetWindowSurface(*win);
+    if(!(*surface))
+    {
+        printf("SDL_CreateSurface Error: %s\n", SDL_GetError());
+        return -1;
+    }
+    return 1;
+}
+void HandleInput(SDL_Event* ev)
+{
+        switch(ev->type)
+        {
+            case SDL_KEYDOWN:
+            {
+                switch (ev->key.keysym.sym)
+                {
+                case SDLK_w:
+                {
+                }
+                break;
+                case SDLK_s:
+                {
+                }
+                break;
+            }
+            break;
+            case SDL_QUIT:
+            {
+                quit = 1;
+                break;
+            }
+        }
+    }
 }
