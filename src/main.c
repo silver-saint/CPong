91278a36031657fdd7a5683e0443ad4e88b276e5
Neyko Naydenov <naydenovneyko@gmail.com>
2025-01-16 11:26:03 -0200

    fixed event pooling

--- src/main.c
+++ src/main.c
@@ -10,12 +10,12 @@
 #define PADDLE_WIDTH 25
 #define PADDLE_HEIGHT 100
 #define PADDING 50
-#define PADDLE_SPEED 50
+#define PADDLE_SPEED 0.8
 //movement
+float  VELOCITY = 0;
 float PLAYER_POSITION_Y = 0; 
 //has game ended
 int quit = 0;
-
 
 
 int InitSDL(SDL_Window** win, SDL_Renderer** renderer, SDL_Surface** surface);
@@ -25,7 +25,7 @@ int main()
     SDL_Window* win = NULL;
     SDL_Renderer* renderer = NULL;
     SDL_Surface* surface = NULL;
-    int32_t init = InitSDL(&win, &renderer, &surface);
+    uint8_t init = InitSDL(&win, &renderer, &surface);
    if(!init)
    {
         SDL_Quit();
@@ -34,24 +34,30 @@ int main()
     SDL_Rect p1;
     SDL_Rect p2;
     SDL_Event ev;
-    while(!quit)
+    Uint64 NOW = SDL_GetPerformanceCounter();
+    Uint64 LAST = 0;
+    double deltaTime = 0;
+    while (!quit)
     {
-    while (SDL_PollEvent(&ev))
-    {
-        HandleInput(&ev);
-      
-
+        if(SDL_PollEvent(&ev))
+        {
+            HandleInput(&ev);
+        }
+        LAST = NOW;
+        NOW = SDL_GetPerformanceCounter();
+        deltaTime = ((NOW - LAST)*1000 / (double)SDL_GetPerformanceFrequency() );
+  //      printf("%lf %f\n", deltaTime, PLAYER_POSITION_Y );
+        PLAYER_POSITION_Y += (VELOCITY * PADDLE_SPEED * deltaTime);
         //render fun;
         SDL_SetRenderDrawColor(renderer, 0x00000000,0x0000000000,0x0000000000,0x0000000000);
         SDL_RenderClear(renderer);
         SDL_SetRenderDrawColor( renderer, 0xFF, 0xFF, 0xFF, 0xFF);
-        DrawPlayer(&p1, 0, (WINDOW_HEIGHT / 2 - PADDING), PADDLE_WIDTH, PADDLE_HEIGHT);
+        DrawPlayer(&p1, 0, (WINDOW_HEIGHT / 2.0f - PADDING) + PLAYER_POSITION_Y, PADDLE_WIDTH, PADDLE_HEIGHT);
         DrawPlayer(&p2, (WINDOW_WIDTH - PADDLE_WIDTH + 1), (WINDOW_HEIGHT / 2 - PADDING), PADDLE_WIDTH, PADDLE_HEIGHT);
         SDL_RenderFillRect( renderer, &p1);
         SDL_RenderFillRect( renderer, &p2);
         SDL_RenderPresent(renderer);
         }
-    }
     SDL_DestroyWindow(win);
     SDL_Quit();
     return 0;
@@ -95,10 +101,12 @@ void HandleInput(SDL_Event* ev)
                 {
                 case SDLK_w:
                 {
+                        VELOCITY = -1;
                 }
                 break;
                 case SDLK_s:
                 {
+                    VELOCITY = 1;
                 }
                 break;
             }
